916a3e4ca5b253e7e7551ff7f6db0d3be8e8e4d2
yoogy0225 <147108401+yoogy0225@users.noreply.github.com>
2023-12-27 19:44:27 +0900

    231227 그래프 소스
    기존의 분석 그래프(막대 그래프, 축적 막대 그래프, 도넛 그래프)와 워드 클라우드, 워드맵 작성한 코드입니다.
    내일 그림이 아는 일반 차트 작성해서 올리겠습니다.

---
+++ src/main/resources/wordData.js
@@ -0,0 +1,249 @@
+var NETWORK_DATA = {
+    "nodes": [
+        {
+            "id": "물가",
+            "value": 10,
+            "group": null
+        },
+        {
+            "id": "고용",
+            "value": 3,
+            "group": "neut"
+        },
+        {
+            "id": "물가상승",
+            "value": 3,
+            "group": "neg"
+        },
+        {
+            "id": "부동산",
+            "value": 4,
+            "group": "neut"
+        },
+        {
+            "id": "안정화",
+            "value": 4,
+            "group": "pos"
+        },
+        {
+            "id": "물가안정",
+            "value": 4,
+            "group": "pos"
+        },
+        {
+            "id": "불안정",
+            "value": 2,
+            "group": "neg"
+        },
+        {
+            "id": "금리폭등",
+            "value": 3,
+            "group": "neg"
+        },
+        {
+            "id": "임금인상",
+            "value": 4,
+            "group": "pos"
+        },
+        {
+            "id": "채소",
+            "value": 2,
+            "group": "neut"
+        },
+        {
+            "id": "병해",
+            "value": 6,
+            "group": "neg"
+        },
+        {
+            "id": "가뭄",
+            "value": 5,
+            "group": "neg"
+        },
+        {
+            "id": "유제품",
+            "value": 5,
+            "group": "neut"
+        },
+        {
+            "id": "자연재해",
+            "value": 3,
+            "group": "neg"
+        },
+        {
+            "id": "농수산물가격완화",
+            "value": 5,
+            "group": "pos"
+        },
+        {
+            "id": "금리",
+            "value": 2,
+            "group": "neut"
+        },
+        {
+            "id": "인플레이션",
+            "value": 4,
+            "group": "neg"
+        },
+        {
+            "id": "부채",
+            "value": 5,
+            "group": "neg"
+        },
+        {
+            "id": "실업",
+            "value": 5,
+            "group": "neg"
+        },
+        {
+            "id": "기근",
+            "value": 2,
+            "group": "neg"
+        },
+        {
+            "id": "완화정책",
+            "value": 3,
+            "group": "pos"
+        },
+        {
+            "id": "구직난",
+            "value": 4,
+            "group": "neg"
+        },
+        {
+            "id": "청년고용안정",
+            "value": 3,
+            "group": "pos"
+        },
+        {
+            "id": "집값",
+            "value": 3,
+            "group": "neut"
+        },
+        {
+            "id": "신용불량자",
+            "value": 2,
+            "group": "neg"
+        },
+        {
+            "id": "고용불안정",
+            "value": 1,
+            "group": "neg"
+        }
+    ],
+
+
+    "links": [
+        {
+            "source": "물가",
+            "target": "집값",
+            "value": 2
+        },
+        {
+            "source": "물가",
+            "target": "인플레이션",
+            "value": 8
+        },
+        {
+            "source": "물가",
+            "target": "채소",
+            "value": 4
+        },
+        {
+            "source": "물가",
+            "target": "유제품",
+            "value": 10
+        },
+        {
+            "source": "물가",
+            "target": "금리",
+            "value": 4
+        },
+        {
+            "source": "물가",
+            "target": "금리폭등",
+            "value": 3
+        },
+        {
+            "source": "물가",
+            "target": "실업",
+            "value": 2
+        },
+        {
+            "source": "물가",
+            "target": "농수산물가격완화",
+            "value": 1
+        },
+        {
+            "source": "물가",
+            "target": "불안정",
+            "value": 4
+        },
+        {
+            "source": "물가",
+            "target": "완화정책",
+            "value": 5
+        },
+        {
+            "source": "물가",
+            "target": "기근",
+            "value": 1
+        },
+        {
+            "source": "물가",
+            "target": "채소",
+            "value": 6
+        },
+        {
+            "source": "채소",
+            "target": "병해",
+            "value": 2
+        },
+        {
+            "source": "채소",
+            "target": "가뭄",
+            "value": 8
+        },
+        {
+            "source": "실업",
+            "target": "고용불안정",
+            "value": 4
+        },
+        {
+            "source": "고용불안정",
+            "target": "청년고용안정",
+            "value": 10
+        },
+        {
+            "source": "물가",
+            "target": "부채",
+            "value": 4
+        },
+        {
+            "source": "부채",
+            "target": "신용불량자",
+            "value": 3
+        },
+        {
+            "source": "고용불안정",
+            "target": "구직난",
+            "value": 2
+        },
+        {
+            "source": "채소",
+            "target": "자연재해",
+            "value": 1
+        },
+        {
+            "source": "물가",
+            "target": "부동산",
+            "value": 4
+        },
+        {
+            "source": "부동산",
+            "target": "안정화",
+            "value": 5
+        },
+
+    ]
+};
